fc1de4e7c1707d8e033fee5907d80578e7394ca8
Samuel Bancroft <spiruel@gmail.com>
2022-05-12 11:50:24 +0100

    second commit

--- train.py
+++ train.py
@@ -4,33 +4,6 @@ import os
 os.environ["WANDB_MODE"] = "offline"
 wandb.init(project="test-project", entity="sciml-leeds")
 
-# Copyright (C) 2020 Michael Mommert
-# This file is part of IndustrialSmokePlumeDetection
-# <https://github.com/HSG-AIML/IndustrialSmokePlumeDetection>.
-#
-# IndustrialSmokePlumeDetection is free software: you can redistribute it
-# and/or modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation, either version 3 of the License,
-# or (at your option) any later version.
-#
-# IndustrialSmokePlumeDetection is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with IndustrialSmokePlumeDetection.  If not,
-# see <http://www.gnu.org/licenses/>.
-#
-# If you use this code for your own project, please cite the following
-# conference contribution:
-#   Mommert, M., Sigel, M., Neuhausler, M., Scheibenreif, L., Borth, D.,
-#   "Characterization of Industrial Smoke Plumes from Remote Sensing Data",
-#   Tackling Climate Change with Machine Learning workshop at NeurIPS 2020.
-#
-#
-# This file contains a wrapper for the training of the segmentation model.
-
 import numpy as np
 import torch
 from torch import nn, optim
